698f22f1f503da8d487bad35276eec73a750ab95
7oiden <7oiden@gmail.com>
2022-05-02 15:38:05 +0200

    connected to api

--- src/components/pages/Overview.js
+++ src/components/pages/Overview.js
@@ -1,3 +1,8 @@
+import { useState, useEffect } from "react";
+import { PRODUCTS_URL } from "../../constants/api";
+import axios from "axios";
+import Loader from "../common/Loader";
+import AlertMessage from "../common/AlertMessage";
 import Layout from "../layout/Layout";
 import Wrapper from "../layout/Wrapper";
 import Container from "react-bootstrap/Container";
@@ -11,6 +16,40 @@ import hotel2 from "../../images/hotel-2.jpg";
 import hotel3 from "../../images/hotel-3.jpg";
 
 function Overview() {
+  const [product, setproduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const url = PRODUCTS_URL;
+
+  useEffect(function () {
+    async function getproduct() {
+      try {
+        const response = await axios.get(url);
+        console.log("response", response.data);
+        setproduct(response.data);
+      } catch (error) {
+        console.log(error);
+        setError(error.toString());
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getproduct();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) return <Loader />;
+
+  if (error) {
+    return (
+      <AlertMessage
+        variant="danger"
+        message="An error occured when trying to fetch the API"
+      />
+    );
+  }
   return (
     <Layout>
       <div className="overview__bg-image"></div>
@@ -21,56 +60,17 @@ function Overview() {
         </div>
         <Container fluid>
           <Row className="gy-5">
-            <ProductCard
-              key="33"
-              image={hotel1}
-              name="Radisson Blu Royal"
-              location="Dreggsallmenningen 1"
-              rating="4.62"
-              reviews="32"
-              category="Hotel"
-              price="1500"
-            />
-            <ProductCard
-              key="34"
-              image={hotel2}
-              name="Scandic Ørnen"
-              location="Lars Hilles gate 18"
-              rating="4.85"
-              reviews="24"
-              category="Hotel"
-              price="2249"
-            />
-            <ProductCard
-              key="35"
-              image={hotel3}
-              name="Augustin Hotel"
-              location="C.Sundts Gate 22"
-              rating="4.71"
-              reviews="41"
-              category="Hotel"
-              price="1340"
-            />
-            <ProductCard
-              key="35"
-              image={hotel3}
-              name="Augustin Hotel"
-              location="C.Sundts Gate 22"
-              rating="4.71"
-              reviews="41"
-              category="Hotel"
-              price="1340"
-            />
-            <ProductCard
-              key="35"
-              image={hotel3}
-              name="Augustin Hotel"
-              location="C.Sundts Gate 22"
-              rating="4.71"
-              reviews="41"
-              category="Hotel"
-              price="1340"
-            />
+            {product.map((item) => {
+              return (
+                <ProductCard
+                  key={item.id}
+                  name={item.name}
+                  image={item.images[0].src}
+                  category={item.categories[0].name}
+                  price={item.prices.price}
+                />
+              );
+            })}
             <ProductCard
               key="35"
               image={hotel3}
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,6 @@
 export const WP_BASE_URL =
   "http://localhost/react-crud/wordpress-5.9.2/wordpress/wp-json/";
+
 export const TOKEN_PATH = "jwt-auth/v1/token";
+
+export const PRODUCTS_URL = "https://holidaze.7oiden.com/wp-json/wc/store/products";
